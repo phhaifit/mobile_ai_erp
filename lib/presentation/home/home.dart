47dbbfc09b43a8734c6d02f1136cf1b5149e8da0
Ngo Thanh Phuong Duong <duongntp@luminpdf.com>
2026-03-21 22:27:13 +0700

    feat(fulfillment): add fulfillment entry point to home screen
    Add order fulfillment IconButton to AppBar actions for navigating
    to the fulfillment list screen.

--- lib/presentation/home/home.dart
+++ lib/presentation/home/home.dart
@@ -67,6 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
     return <Widget>[
       _buildProductMetadataButton(),
       _buildOrderTrackingButton(),
+      _buildFulfillmentButton(),
       _buildLanguageButton(),
       _buildThemeButton(),
       _buildLogoutButton(),
@@ -80,6 +81,13 @@ class _HomeScreenState extends State<HomeScreen> {
         ProductMetadataNavigator.openProductMetadataHome(context);
       },
       icon: const Icon(Icons.dashboard_outlined),
+  Widget _buildFulfillmentButton() {
+    return IconButton(
+      tooltip: 'Order Fulfillment',
+      onPressed: () {
+        Navigator.of(context).pushNamed(Routes.fulfillment);
+      },
+      icon: const Icon(Icons.local_shipping_outlined),
     );
   }
 
