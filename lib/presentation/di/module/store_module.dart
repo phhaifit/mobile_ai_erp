0af1105c4d9d14d5003439b64fd7cc760ca9b28e
Tri Nguyen <minhtri257tk@gmail.com>
2026-03-22 21:35:57 +0700

    refactor: reorganize imports and update store registrations in StoreModule

--- lib/presentation/di/module/store_module.dart
+++ lib/presentation/di/module/store_module.dart
@@ -3,18 +3,12 @@ import 'dart:async';
 import 'package:mobile_ai_erp/core/stores/error/error_store.dart';
 import 'package:mobile_ai_erp/core/stores/form/form_store.dart';
 import 'package:mobile_ai_erp/domain/repository/customer/customer_repository.dart';
-import 'package:mobile_ai_erp/domain/repository/setting/setting_repository.dart';
-import 'package:mobile_ai_erp/presentation/customer_management/store/customer_store.dart';
 import 'package:mobile_ai_erp/domain/repository/product_metadata/product_metadata_repository.dart';
+import 'package:mobile_ai_erp/domain/repository/setting/setting_repository.dart';
 import 'package:mobile_ai_erp/domain/usecase/post/get_post_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/user/is_logged_in_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/user/login_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/user/save_login_in_status_usecase.dart';
-import 'package:mobile_ai_erp/presentation/home/store/language/language_store.dart';
-import 'package:mobile_ai_erp/presentation/home/store/theme/theme_store.dart';
-import 'package:mobile_ai_erp/presentation/login/store/login_store.dart';
-import 'package:mobile_ai_erp/presentation/post/store/post_store.dart';
-import 'package:mobile_ai_erp/domain/usecase/post/get_post_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/web_builder/apply_web_theme_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/web_builder/delete_cms_page_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/web_builder/get_cms_page_by_id_usecase.dart';
@@ -24,10 +18,14 @@ import 'package:mobile_ai_erp/domain/usecase/web_builder/get_web_theme_by_id_use
 import 'package:mobile_ai_erp/domain/usecase/web_builder/get_web_themes_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/web_builder/save_cms_page_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/web_builder/save_store_settings_usecase.dart';
+import 'package:mobile_ai_erp/presentation/customer_management/store/customer_store.dart';
+import 'package:mobile_ai_erp/presentation/home/store/language/language_store.dart';
+import 'package:mobile_ai_erp/presentation/home/store/theme/theme_store.dart';
+import 'package:mobile_ai_erp/presentation/login/store/login_store.dart';
 import 'package:mobile_ai_erp/presentation/post/store/post_store.dart';
+import 'package:mobile_ai_erp/presentation/product_metadata/store/product_metadata_store.dart';
 import 'package:mobile_ai_erp/presentation/reports/data/reports_mock_repository.dart';
 import 'package:mobile_ai_erp/presentation/reports/store/reports_store.dart';
-import 'package:mobile_ai_erp/presentation/product_metadata/store/product_metadata_store.dart';
 import 'package:mobile_ai_erp/presentation/web_builder/store/cms_page_store.dart';
 import 'package:mobile_ai_erp/presentation/web_builder/store/store_settings_store.dart';
 import 'package:mobile_ai_erp/presentation/web_builder/store/web_theme_store.dart';
@@ -54,6 +52,7 @@ class StoreModule {
         getIt<ErrorStore>(),
       ),
     );
+
     getIt.registerSingleton<PostStore>(
       PostStore(
         getIt<GetPostUseCase>(),
@@ -75,9 +74,9 @@ class StoreModule {
       ),
     );
 
-    getIt.registerSingleton<PostStore>(
-      PostStore(
-        getIt<GetPostUseCase>(),
+    getIt.registerSingleton<CustomerStore>(
+      CustomerStore(
+        getIt<CustomerRepository>(),
         getIt<ErrorStore>(),
       ),
     );
@@ -96,9 +95,6 @@ class StoreModule {
       ),
     );
 
-    getIt.registerSingleton<CustomerStore>(
-      CustomerStore(
-        getIt<CustomerRepository>(),
     // web_builder stores:------------------------------------------------------
     getIt.registerSingleton<CmsPageStore>(
       CmsPageStore(
