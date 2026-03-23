cfa6f1340fb965430542c64513d86339c59d461c
Hoàng Túy Minh <tuyminh.hoang@gmail.com>
2026-03-24 03:19:32 +0700

    product listing item basic widget
    contains basic information for customer
    no styling/spacing applied yet

---
+++ lib/presentation/storefront/product_listing_item.dart
@@ -0,0 +1,58 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:mobile_ai_erp/domain/entity/product/product.dart';
+
+/// Widget for a product shown in homepage, product listing page (with or without search/filters), brand and collection landing pages
+/// 
+/// Product information provided by parent widget
+class ProductListingItem extends StatelessWidget {
+  const ProductListingItem({
+    super.key,
+    required this.productListing,
+  });
+
+  final Product productListing;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 5.0, bottom: 5.0),
+      child: Row(
+        children: [
+          productListing.imageSource != null ? 
+            Image.network(
+              productListing.imageSource!,
+              
+            ) : 
+            Image.network('https://picsum.photos/250?image=9'),
+          Column(
+            children: [
+              Text(
+                productListing.productName,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+              Row(
+                children: [
+                  Text(productListing.category.name),
+                  Text(productListing.brand.name)
+                ],
+              ),
+              Row(
+                children: [
+                  Text(productListing.rating.toString()), // temporary until rating image is available
+                  Text(
+                    productListing.price.toString(),
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Text(productListing.currency)
+                ],
+              )
+            ]
+          )
+        ],
+      ),
+    );
+  }
+
+}
