24e77e08151cb409037ead36e397b757e4d5a16a
Ngo Thanh Phuong Duong <duongntp@luminpdf.com>
2026-04-11 10:54:15 +0700

    refactor(fulfillment): remove unused use cases and simplify store DI wiring
    - Delete UpdatePickedQuantityUseCase (BE does not track per-item picking)
    - Delete AddPackageUseCase (BE does not have packaging API yet)
    - Delete UpdatePackageUseCase (BE does not have packaging API yet)
    - Simplify UpdateFulfillmentStatusUseCase (delegate validation to BE API)
    - Remove updatePickedQuantity, addPackage, updatePackage actions from FulfillmentStore
    - Reduce FulfillmentStore constructor from 7 to 4 params
    - Clean up DI registrations in usecase_module.dart and store_module.dart

--- lib/presentation/order_fulfillment/store/fulfillment_store.dart
+++ lib/presentation/order_fulfillment/store/fulfillment_store.dart
@@ -1,13 +1,9 @@
 import 'package:mobile_ai_erp/core/stores/error/error_store.dart';
 import 'package:mobile_ai_erp/domain/entity/fulfillment/fulfillment_order.dart';
 import 'package:mobile_ai_erp/domain/entity/fulfillment/fulfillment_status.dart';
-import 'package:mobile_ai_erp/domain/entity/fulfillment/package_info.dart';
-import 'package:mobile_ai_erp/domain/usecase/fulfillment/add_package_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/fulfillment/get_fulfillment_order_detail_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/fulfillment/get_fulfillment_orders_usecase.dart';
 import 'package:mobile_ai_erp/domain/usecase/fulfillment/update_fulfillment_status_usecase.dart';
-import 'package:mobile_ai_erp/domain/usecase/fulfillment/update_package_usecase.dart';
-import 'package:mobile_ai_erp/domain/usecase/fulfillment/update_picked_quantity_usecase.dart';
 import 'package:mobx/mobx.dart';
 
 part 'fulfillment_store.g.dart';
@@ -18,18 +14,12 @@ abstract class _FulfillmentStore with Store {
   final GetFulfillmentOrdersUseCase _getOrdersUseCase;
   final GetFulfillmentOrderDetailUseCase _getOrderDetailUseCase;
   final UpdateFulfillmentStatusUseCase _updateStatusUseCase;
-  final UpdatePickedQuantityUseCase _updatePickedQtyUseCase;
-  final AddPackageUseCase _addPackageUseCase;
-  final UpdatePackageUseCase _updatePackageUseCase;
   final ErrorStore errorStore;
 
   _FulfillmentStore(
     this._getOrdersUseCase,
     this._getOrderDetailUseCase,
     this._updateStatusUseCase,
-    this._updatePickedQtyUseCase,
-    this._addPackageUseCase,
-    this._updatePackageUseCase,
     this.errorStore,
   );
 
@@ -95,47 +85,6 @@ abstract class _FulfillmentStore with Store {
       );
       await getOrderDetail(orderId);
       await getOrders();
-    } catch (e) {
-      errorStore.errorMessage = e.toString();
-    }
-  }
-
-  @action
-  Future<void> updatePickedQuantity(
-      String orderId, String itemId, int qty) async {
-    try {
-      await _updatePickedQtyUseCase.call(
-        params: UpdatePickedQuantityParams(
-          orderId: orderId,
-          itemId: itemId,
-          quantity: qty,
-        ),
-      );
-      await getOrderDetail(orderId);
-    } catch (e) {
-      errorStore.errorMessage = e.toString();
-    }
-  }
-
-  @action
-  Future<void> addPackage(String orderId, PackageInfo package) async {
-    try {
-      await _addPackageUseCase.call(
-        params: AddPackageParams(orderId: orderId, package: package),
-      );
-      await getOrderDetail(orderId);
-    } catch (e) {
-      errorStore.errorMessage = e.toString();
-    }
-  }
-
-  @action
-  Future<void> updatePackage(String orderId, PackageInfo package) async {
-    try {
-      await _updatePackageUseCase.call(
-        params: UpdatePackageParams(orderId: orderId, package: package),
-      );
-      await getOrderDetail(orderId);
     } catch (e) {
       errorStore.errorMessage = e.toString();
     }
