a1c318d0cc03a3fcfd0fadbf4ace3646563b3509
NGUYEN NGO NGOC NHU <nguyenngongocnhu96@gmail.com>
2026-04-17 09:29:01 +0700

    feat(supplier): add ProductSupplierLinkDto and SupplierResponseDto; implement mappers for product-supplier relationships

--- lib/domain/entity/supplier/supplier_upsert_payload.dart
+++ lib/domain/entity/supplier/supplier_upsert_payload.dart
@@ -1,25 +1,40 @@
 class SupplierUpsertPayload {
   final String code;
   final String name;
-  final String phone;
-  final String email;
-  final String address;
-  final String taxCode;
-  final String idCard;
-  final String bankName;
-  final String bankAccount;
-  final String bankNote;
+  final String? phone;
+  final String? email;
+  final String? address;
+  final String? taxCode;
+  final String? idCard;
+  final String? bankName;
+  final String? bankAccount;
+  final String? bankNote;
 
   const SupplierUpsertPayload({
     required this.code,
     required this.name,
-    this.phone = '',
-    this.email = '',
-    this.address = '',
-    this.taxCode = '',
-    this.idCard = '',
-    this.bankName = '',
-    this.bankAccount = '',
-    this.bankNote = '',
+    this.phone,
+    this.email,
+    this.address,
+    this.taxCode,
+    this.idCard,
+    this.bankName,
+    this.bankAccount,
+    this.bankNote,
   });
+
+  Map<String, dynamic> toJson() {
+    return {
+      'code': code,
+      'name': name,
+      'phone': phone,
+      'email': email,
+      'address': address,
+      'taxCode': taxCode,
+      'idCard': idCard,
+      'bankName': bankName,
+      'bankAccount': bankAccount,
+      'bankNote': bankNote,
+    };
+  }
 }
