a79d22e108a7f961359bb28daf7becb29d98809c
nlanhduy <duyzhii@gmail.com>
2026-05-01 14:49:44 +0700

    feat(customer): add 15 customer use cases

---
+++ lib/domain/usecase/customer/get_customer_groups_usecase.dart
@@ -0,0 +1,37 @@
+import 'package:mobile_ai_erp/core/domain/usecase/use_case.dart';
+import 'package:mobile_ai_erp/domain/repository/customer/customer_repository.dart';
+
+class GetCustomerGroupsParams {
+  final int page;
+  final int pageSize;
+  final String? search;
+  final String? sortBy;
+  final String? sortOrder;
+
+  const GetCustomerGroupsParams({
+    this.page = 1,
+    this.pageSize = 20,
+    this.search,
+    this.sortBy,
+    this.sortOrder,
+  });
+}
+
+class GetCustomerGroupsUseCase
+    extends UseCase<CustomerGroupListResult, GetCustomerGroupsParams> {
+  final CustomerRepository _repository;
+
+  GetCustomerGroupsUseCase(this._repository);
+
+  @override
+  Future<CustomerGroupListResult> call(
+      {required GetCustomerGroupsParams params}) {
+    return _repository.getCustomerGroups(
+      page: params.page,
+      pageSize: params.pageSize,
+      search: params.search,
+      sortBy: params.sortBy,
+      sortOrder: params.sortOrder,
+    );
+  }
+}
---
+++ lib/domain/usecase/customer/get_customers_usecase.dart
@@ -0,0 +1,35 @@
+import 'package:mobile_ai_erp/core/domain/usecase/use_case.dart';
+import 'package:mobile_ai_erp/domain/repository/customer/customer_repository.dart';
+
+class GetCustomersParams {
+  final int page;
+  final int pageSize;
+  final String? search;
+  final String? status;
+  final String? groupId;
+
+  const GetCustomersParams({
+    this.page = 1,
+    this.pageSize = 20,
+    this.search,
+    this.status,
+    this.groupId,
+  });
+}
+
+class GetCustomersUseCase extends UseCase<CustomerListResult, GetCustomersParams> {
+  final CustomerRepository _repository;
+
+  GetCustomersUseCase(this._repository);
+
+  @override
+  Future<CustomerListResult> call({required GetCustomersParams params}) {
+    return _repository.getCustomers(
+      page: params.page,
+      pageSize: params.pageSize,
+      search: params.search,
+      status: params.status,
+      groupId: params.groupId,
+    );
+  }
+}
