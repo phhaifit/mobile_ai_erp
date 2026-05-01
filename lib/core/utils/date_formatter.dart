5c46db1857bfe21a9a95fdcf20fc5639aa28d6be
NGUYEN NGO NGOC NHU <nguyenngongocnhu96@gmail.com>
2026-05-01 13:02:55 +0700

    fix(product-metadata): ensure date formatting uses local time

--- lib/core/utils/date_formatter.dart
+++ lib/core/utils/date_formatter.dart
@@ -2,14 +2,14 @@ import 'package:intl/intl.dart';
 
 class DateFormatter {
   static String formatFull(DateTime dateTime) {
-    return DateFormat('MMM dd, yyyy hh:mm a').format(dateTime);
+    return DateFormat('MMM dd, yyyy hh:mm a').format(dateTime.toLocal());
   }
 
   static String formatDate(DateTime dateTime) {
-    return DateFormat('MMM dd, yyyy').format(dateTime);
+    return DateFormat('MMM dd, yyyy').format(dateTime.toLocal());
   }
 
   static String formatTime(DateTime dateTime) {
-    return DateFormat('hh:mm a').format(dateTime);
+    return DateFormat('hh:mm a').format(dateTime.toLocal());
   }
 }
